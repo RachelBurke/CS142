e6013cde28c4d01c69440ffdedd8fec081cf87b7
Rachel Burke <31460482+RachelBurke@users.noreply.github.com>
2018-11-26 22:04:26 -0500

    Update and Move to Exam1 Directory

--- Exam1/exam problem.cpp
+++ Exam1/exam problem.cpp
@@ -1,3 +1,15 @@
+//This was an exam question. The program needed to print out oolongtea every 5th number or
+// 1
+// 2 
+// 3
+// 4
+// 5 oolongtea
+// 6
+// 7
+// ...
+// 10 oolongtea
+// ...
+
 #include <iostream>
 using namespace std;
 
